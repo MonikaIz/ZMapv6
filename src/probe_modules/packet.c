8e72eb80e5d468138e204d120dcb0888598ad26e
MOnika <izdebskam93@gmail.com>
2018-11-18 21:15:45 +0100

    Init

---
+++ src/probe_modules/packet.c
@@ -0,0 +1,188 @@
+/*
+ * ZMap Copyright 2013 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#include "../../lib/includes.h"
+#include "../../lib/xalloc.h"
+#include "packet.h"
+
+#include "../state.h"
+
+#ifndef NDEBUG
+void print_macaddr(struct ifreq *i)
+{
+	printf("Device %s -> Ethernet %02x:%02x:%02x:%02x:%02x:%02x\n",
+	       i->ifr_name, (int)((unsigned char *)&i->ifr_addr.sa_data)[0],
+	       (int)((unsigned char *)&i->ifr_addr.sa_data)[1],
+	       (int)((unsigned char *)&i->ifr_addr.sa_data)[2],
+	       (int)((unsigned char *)&i->ifr_addr.sa_data)[3],
+	       (int)((unsigned char *)&i->ifr_addr.sa_data)[4],
+	       (int)((unsigned char *)&i->ifr_addr.sa_data)[5]);
+}
+#endif /* NDEBUG */
+
+#define IP_ADDR_LEN_STR 20
+
+void fprintf_ip_header(FILE *fp, struct ip *iph)
+{
+	struct in_addr *s = (struct in_addr *)&(iph->ip_src);
+	struct in_addr *d = (struct in_addr *)&(iph->ip_dst);
+
+	char srcip[IP_ADDR_LEN_STR + 1];
+	char dstip[IP_ADDR_LEN_STR + 1];
+	// inet_ntoa is a const char * so we if just call it in
+	// fprintf, you'll get back wrong results since we're
+	// calling it twice.
+	strncpy(srcip, inet_ntoa(*s), IP_ADDR_LEN_STR - 1);
+	strncpy(dstip, inet_ntoa(*d), IP_ADDR_LEN_STR - 1);
+
+	srcip[IP_ADDR_LEN_STR] = '\0';
+	dstip[IP_ADDR_LEN_STR] = '\0';
+
+	fprintf(fp, "ip { saddr: %s | daddr: %s | checksum: %#04X }\n", srcip,
+		dstip, ntohs(iph->ip_sum));
+}
+
+void fprintf_ipv6_header(FILE *fp, struct ip6_hdr *iph)
+{
+	struct in6_addr *s = (struct in6_addr *) &(iph->ip6_src);
+	struct in6_addr *d = (struct in6_addr *) &(iph->ip6_dst);
+
+	char srcip[INET6_ADDRSTRLEN+1];
+	char dstip[INET6_ADDRSTRLEN+1];
+	unsigned char next = (unsigned char) (iph->ip6_nxt);
+
+	// TODO: Is restrict correct here?
+	inet_ntop(AF_INET6, s, (char * restrict) &srcip, INET6_ADDRSTRLEN);
+	inet_ntop(AF_INET6, d, (char * restrict) &dstip, INET6_ADDRSTRLEN);
+
+	srcip[INET6_ADDRSTRLEN] = '\0';
+	dstip[INET6_ADDRSTRLEN] = '\0';
+
+	fprintf(fp, "ip6 { saddr: %s | daddr: %s | nxthdr: %u }\n",
+			srcip,
+			dstip,
+			next);
+}
+
+void fprintf_eth_header(FILE *fp, struct ether_header *ethh)
+{
+	fprintf(fp,
+		"eth { shost: %02x:%02x:%02x:%02x:%02x:%02x | "
+		"dhost: %02x:%02x:%02x:%02x:%02x:%02x }\n",
+		(int)((unsigned char *)ethh->ether_shost)[0],
+		(int)((unsigned char *)ethh->ether_shost)[1],
+		(int)((unsigned char *)ethh->ether_shost)[2],
+		(int)((unsigned char *)ethh->ether_shost)[3],
+		(int)((unsigned char *)ethh->ether_shost)[4],
+		(int)((unsigned char *)ethh->ether_shost)[5],
+		(int)((unsigned char *)ethh->ether_dhost)[0],
+		(int)((unsigned char *)ethh->ether_dhost)[1],
+		(int)((unsigned char *)ethh->ether_dhost)[2],
+		(int)((unsigned char *)ethh->ether_dhost)[3],
+		(int)((unsigned char *)ethh->ether_dhost)[4],
+		(int)((unsigned char *)ethh->ether_dhost)[5]);
+}
+
+void make_eth_header(struct ether_header *ethh, macaddr_t *src, macaddr_t *dst)
+{
+	// Create a frame with IPv4 ethertype by default
+	make_eth_header_ethertype(ethh, src, dst, ETHERTYPE_IP);
+}
+
+void make_eth_header_ethertype(struct ether_header *ethh, macaddr_t *src, macaddr_t *dst, uint16_t ethertype)
+{
+	memcpy(ethh->ether_shost, src, ETHER_ADDR_LEN);
+	memcpy(ethh->ether_dhost, dst, ETHER_ADDR_LEN);
+	ethh->ether_type = htons(ethertype);
+}
+
+void make_ip_header(struct ip *iph, uint8_t protocol, uint16_t len)
+{
+	iph->ip_hl = 5;  // Internet Header Length
+	iph->ip_v = 4;   // IPv4
+	iph->ip_tos = 0; // Type of Service
+	iph->ip_len = len;
+	iph->ip_id = htons(54321); // identification number
+	iph->ip_off = 0;	   // fragmentation flag
+	iph->ip_ttl = MAXTTL;      // time to live (TTL)
+	iph->ip_p = protocol;      // upper layer protocol => TCP
+	// we set the checksum = 0 for now because that's
+	// what it needs to be when we run the IP checksum
+	iph->ip_sum = 0;
+}
+
+void make_ip6_header(struct ip6_hdr *iph, uint8_t protocol, uint16_t len)
+{
+	iph->ip6_ctlun.ip6_un2_vfc = 0x60; // 4 bits version, top 4 bits class
+	iph->ip6_ctlun.ip6_un1.ip6_un1_plen = htons(len); // payload length
+	iph->ip6_ctlun.ip6_un1.ip6_un1_nxt = protocol; // next header
+	iph->ip6_ctlun.ip6_un1.ip6_un1_hlim = MAXTTL; // hop limit
+}
+void make_icmp6_header(struct icmp6_hdr *buf)
+{
+    buf->icmp6_type = ICMP6_ECHO_REQUEST;
+    buf->icmp6_code = 0;
+    buf->icmp6_cksum = 0;
+    // buf->icmp_seq = 0;
+    // TODO: Set ICMP ECHO REQ specific fields
+}
+
+void make_icmp_header(struct icmp *buf)
+{
+	buf->icmp_type = ICMP_ECHO;
+	buf->icmp_code = 0;
+	buf->icmp_seq = 0;
+}
+
+void make_tcp_header(struct tcphdr *tcp_header, port_h_t dest_port,
+		     uint16_t th_flags)
+{
+	tcp_header->th_seq = random();
+	tcp_header->th_ack = 0;
+	tcp_header->th_x2 = 0;
+	tcp_header->th_off = 5; // data offset
+	tcp_header->th_flags = 0;
+	tcp_header->th_flags |= th_flags;
+	tcp_header->th_win = htons(65535); // largest possible window
+	tcp_header->th_sum = 0;
+	tcp_header->th_urp = 0;
+	tcp_header->th_dport = htons(dest_port);
+}
+
+void make_udp_header(struct udphdr *udp_header, port_h_t dest_port,
+		     uint16_t len)
+{
+	udp_header->uh_dport = htons(dest_port);
+	udp_header->uh_ulen = htons(len);
+	// checksum ignored in IPv4 if 0
+	udp_header->uh_sum = 0;
+}
+
+// Note: caller must free return value
+char *make_ip_str(uint32_t ip)
+{
+	struct in_addr t;
+	t.s_addr = ip;
+	const char *temp = inet_ntoa(t);
+	char *retv = xmalloc(strlen(temp) + 1);
+	strcpy(retv, temp);
+	return retv;
+}
+
+// Note: caller must free return value
+char *make_ipv6_str(struct in6_addr *ipv6)
+{
+	char *retv = xmalloc(INET6_ADDRSTRLEN + 1);
+	inet_ntop(AF_INET6, ipv6, retv, INET6_ADDRSTRLEN);
+	return retv;
+}
---
+++ src/recv.c
@@ -0,0 +1,220 @@
+/*
+ * ZMap Copyright 2013 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+#include "recv.h"
+
+#include <assert.h>
+
+#include "../lib/includes.h"
+#include "../lib/logger.h"
+#include "../lib/pbm.h"
+
+#include <pthread.h>
+#include <unistd.h>
+
+#include "recv-internal.h"
+#include "state.h"
+#include "validate.h"
+#include "fieldset.h"
+#include "expression.h"
+#include "probe_modules/probe_modules.h"
+#include "output_modules/output_modules.h"
+
+// bitmap of observed IP addresses
+static uint8_t **seen = NULL;
+
+// IPv6
+static int ipv6 = 0;
+
+void handle_packet(uint32_t buflen, const u_char *bytes)
+{
+	struct ip *ip_hdr;
+	uint32_t src_ip;
+	uint32_t validation[VALIDATE_BYTES / sizeof(uint8_t)];
+
+	// IPv6
+	struct ip6_hdr *ipv6_hdr;
+
+	// IPv6
+	if (ipv6) {
+		if ((sizeof(struct ip6_hdr) + sizeof(struct ether_header)) > buflen) {
+			// buffer not large enough to contain ethernet
+			// and ip headers. further action would overrun buf
+			return;
+		}
+		ipv6_hdr = (struct ip6_hdr *) &bytes[sizeof(struct ether_header)];
+
+		validate_gen_ipv6(&ipv6_hdr->ip6_dst, &(ipv6_hdr->ip6_src), (uint8_t *)validation);
+		ip_hdr = (struct ip *) ipv6_hdr;
+	} else {
+		if (sizeof(struct ip) + sizeof(struct ether_header) > buflen) {
+			// buffer not large enough to contain ethernet
+			// and ip headers. further action would overrun buf
+			return;
+		}
+		ip_hdr = (struct ip *)&bytes[sizeof(struct ether_header)];
+
+		src_ip = ip_hdr->ip_src.s_addr;
+
+		// TODO: for TTL exceeded messages, ip_hdr->saddr is going to be
+		// different and we must calculate off potential payload message instead
+		validate_gen(ip_hdr->ip_dst.s_addr, ip_hdr->ip_src.s_addr,
+				 (uint8_t *)validation);
+	}
+
+	if (!zconf.probe_module->validate_packet(
+		ip_hdr, buflen - sizeof(struct ether_header), &src_ip,
+		validation)) {
+		zrecv.validation_failed++;
+		return;
+	} else {
+		zrecv.validation_passed++;
+	}
+
+	// IPv6
+	int is_repeat;
+	if (ipv6) {
+		is_repeat = 0;
+	} else {
+		// woo! We've validated that the packet is a response to our scan
+		is_repeat = pbm_check(seen, ntohl(src_ip));
+		// track whether this is the first packet in an IP fragment.
+		if (ip_hdr->ip_off & IP_MF) {
+			zrecv.ip_fragments++;
+		}
+	}
+
+	fieldset_t *fs = fs_new_fieldset();
+	// IPv6
+	if (ipv6) {
+		fs_add_ipv6_fields(fs, ipv6_hdr);
+	} else {
+		fs_add_ip_fields(fs, ip_hdr);
+	}
+
+	zconf.probe_module->process_packet(bytes, buflen, fs, validation);
+	fs_add_system_fields(fs, is_repeat, zsend.complete);
+	int success_index = zconf.fsconf.success_index;
+	assert(success_index < fs->len);
+	int is_success = fs_get_uint64_by_index(fs, success_index);
+
+	if (is_success) {
+		zrecv.success_total++;
+		if (!is_repeat) {
+			zrecv.success_unique++;
+			pbm_set(seen, ntohl(src_ip));
+		}
+		if (zsend.complete) {
+			zrecv.cooldown_total++;
+			if (!is_repeat) {
+				zrecv.cooldown_unique++;
+			}
+		}
+	} else {
+		zrecv.failure_total++;
+	}
+	// probe module includes app_success field
+	if (zconf.fsconf.app_success_index >= 0) {
+		int is_app_success =
+		    fs_get_uint64_by_index(fs, zconf.fsconf.app_success_index);
+		if (is_app_success) {
+			zrecv.app_success_total++;
+			if (!is_repeat) {
+				zrecv.app_success_unique++;
+			}
+		}
+	}
+
+	fieldset_t *o = NULL;
+	// we need to translate the data provided by the probe module
+	// into a fieldset that can be used by the output module
+	if (!is_success && zconf.filter_unsuccessful) {
+		goto cleanup;
+	}
+	if (is_repeat && zconf.filter_duplicates) {
+		goto cleanup;
+	}
+	if (!evaluate_expression(zconf.filter.expression, fs)) {
+		goto cleanup;
+	}
+	o = translate_fieldset(fs, &zconf.fsconf.translation);
+	if (zconf.output_module && zconf.output_module->process_ip) {
+		zconf.output_module->process_ip(o);
+	}
+cleanup:
+	fs_free(fs);
+	free(o);
+	if (zconf.output_module && zconf.output_module->update &&
+	    !(zrecv.success_unique % zconf.output_module->update_interval)) {
+		zconf.output_module->update(&zconf, &zsend, &zrecv);
+	}
+}
+
+int recv_run(pthread_mutex_t *recv_ready_mutex)
+{
+	// IPv6
+	if (zconf.ipv6_target_filename) {
+		ipv6 = 1;
+	}
+
+	log_trace("recv", "recv thread started");
+	log_debug("recv", "capturing responses on %s", zconf.iface);
+	if (!zconf.dryrun) {
+		recv_init();
+	}
+
+	// initialize paged bitmap
+	seen = pbm_init();
+	if (zconf.filter_duplicates) {
+		log_debug("recv",
+			  "duplicate responses will be excluded from output");
+	} else {
+		log_debug("recv",
+			  "duplicate responses will be included in output");
+	}
+	if (zconf.filter_unsuccessful) {
+		log_debug(
+		    "recv",
+		    "unsuccessful responses will be excluded from output");
+	} else {
+		log_debug("recv",
+			  "unsuccessful responses will be included in output");
+	}
+
+	pthread_mutex_lock(recv_ready_mutex);
+	zconf.recv_ready = 1;
+	pthread_mutex_unlock(recv_ready_mutex);
+	zrecv.start = now();
+	if (zconf.max_results == 0) {
+		zconf.max_results = -1;
+	}
+
+	do {
+		if (zconf.dryrun) {
+			sleep(1);
+		} else {
+			recv_packets();
+			if (zconf.max_results &&
+			    zrecv.success_unique >= zconf.max_results) {
+				break;
+			}
+		}
+	} while (
+	    !(zsend.complete && (now() - zsend.finish > zconf.cooldown_secs)));
+	zrecv.finish = now();
+	// get final pcap statistics before closing
+	recv_update_stats();
+	if (!zconf.dryrun) {
+		pthread_mutex_lock(recv_ready_mutex);
+		recv_cleanup();
+		pthread_mutex_unlock(recv_ready_mutex);
+	}
+	zrecv.complete = 1;
+	log_debug("recv", "thread finished");
+	return 0;
+}
